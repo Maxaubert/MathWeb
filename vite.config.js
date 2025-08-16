f2da830450ac9747bf7ba6e82f95d2c875979a44
Maxaubert <maxaubert@example.com>
2025-08-16 20:44:04 +0200

    Fix base path: MathWeb with capital W to match repository name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mathweb/',
+  base: '/MathWeb/',
   server: {
     port: 3000,
     open: true
